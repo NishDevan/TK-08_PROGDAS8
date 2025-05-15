acbfd588bffaa40fd6dbec060f6e0bee63e06e3e
NishDevan <danishputradevananda1@gmail.com>
2025-05-15 19:42:19 +0700

    Addition done!

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello test 2");
+    int angka1, angka2, pilihan;
+    printf("Selama datang di kalkulator kelompok 'Javascript'!\n");
+
+    printf("Masukkan angka pertama anda : ");
+    scanf("%d", &angka1);
+
+    printf("Masukkan angka kedua anda : ");
+    scanf("%d", &angka2);
+
+    printf("Apa yang anda ingin lakukan? (1: Penjumlahan, 2: Pengurangan, 3: Perkalian, 4: Pembagian) : ");
+    scanf("%d", &pilihan);
+
+    switch (pilihan) {
+        case 1:
+            // Untuk Penjumlahan
+            break;
+        case 2:
+            // Untuk Pengurangan
+            break;
+        case 3:
+            // Untuk Perkalian
+            break;
+        case 4:
+            // Untuk Pembagian
+            break;
+        default:
+            printf("Terjadi kesalahan, mohon untuk mengecek pilihan anda!");
+            break;
+    }
     return 0;
+}
+
+void addition(angka1, angka2) {
+    int hasil;
+
+    hasil = angka1 + angka2;
+    printf("Hasil dari penjumlahan tersebut: %d", hasil);
 }
