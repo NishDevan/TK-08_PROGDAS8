f7cd704c6e217522cda7451200ea2ab467458c71
niel64 <osianturi128@gmail.com>
2025-05-18 08:19:42 +0700

    nambahin subtraction

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(){
             addition(angka1, angka2);
             break;
         case 2:
-            // Untuk Pengurangan
+            subtraction(angka1, angka2);
             break;
         case 3:
             // Untuk Perkalian
@@ -40,4 +40,11 @@ void addition(int angka1, int angka2) {
 
     hasil = angka1 + angka2;
     printf("Hasil dari penjumlahan tersebut: %d", hasil);
+}
+
+void subtraction(int angka1, int angka2) {
+    int hasil;
+
+    hasil = angka1 - angka2;
+    printf("Hasil dari pengurangan tersebut: %d", hasil);
 }
